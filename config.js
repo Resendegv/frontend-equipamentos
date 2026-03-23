6d2ebdbaa8a7d0ecafc9c77b9fcb4a4e5a50f63f
Vinicius Resende <resende-vinicius@hotmail.com>
2026-03-23 17:46:06 -0300

    Atualizando frontend

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-(function () {
-  const isLocal =
-    window.location.hostname === "127.0.0.1" ||
-    window.location.hostname === "localhost";
-
-  window.API_URL = isLocal
-    ? "http://127.0.0.1:8000"
-    : "https://api-equipamentos2.onrender.com";
-})();
+window.API_URL = "https://api-equipamentos2.onrender.com";
